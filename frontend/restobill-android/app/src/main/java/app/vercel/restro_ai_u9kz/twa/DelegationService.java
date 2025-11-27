3c8c1f53fbefa386902fa08f1d4a9de43fce3cf4
ShivShankar Kumar <69760883+shivshankar9@users.noreply.github.com>
2025-11-27 17:52:03 +0530

    s

---
+++ frontend/restobill-android/app/src/main/java/app/vercel/restro_ai_u9kz/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.restro_ai_u9kz.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
